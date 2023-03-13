afd671accf4ea25e8fecfecda457cf815d3166d5
FfrankF <frankh11erich@gmail.com>
2023-03-13 21:33:49 +0100

    update 13/03
    updated and aligned level assets. started work on defending behaviour.

---
+++ Assets/Scripts/AI/State Machine/States/defendingState.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Assets.Scripts.AI.State_Machine.States.Base;
+
+namespace Assets.Scripts.AI.State_Machine.States
+{
+    public class defendingState : State 
+    {
+        private Baller baller;
+        private Vector3 newPos;
+        private Vector3 pos;
+        private Vector3 defendingZonePosition;
+
+        public defendingState(IStateMachineMember owner) : base(owner)
+        {
+            Owner = owner;
+            pos = GameObject.FindWithTag("AI 1").transform.position;
+        }
+ 
+        public override void Enter()
+        {
+            baller = (Owner as Baller); // Set Baller to the object that the script is attached to
+            baller.shouldMove = true;
+        }
+
+        public override void Execute()
+        {
+
+            if (!baller.shouldMove)
+            {
+                baller.StateMachine.ChangeState(new Examine(baller));
+                return; // Stop moving if shouldMove is false
+            }
+        
+            
+            var step = baller.speed * Time.deltaTime;
+            defendingZonePosition = GameObject.FindWithTag("defending Side").transform.position;
+            pos = GameObject.FindWithTag("AI 1").transform.position;
+            // baller.transform.position = Vector3.MoveTowards(pos, basketballPosition, step);
+            baller.navMeshAgent.SetDestination(defendingZonePosition);
+
+
+            if (baller.heldBall) {
+                baller.speed = 0f; // stop moving if ball is held
+                baller.StateMachine.ChangeState(new Examine(baller));
+            }
+
+            Debug.Log(baller.speed);
+        }
+
+        public override void Exit()
+        {
+            
+        }
+         
+        
+    }
+} 
+
