1971fc0090c6dcce3b9476831f79a10fc9a12329
noahbearx3 <noah.b3rkhout@gmail.com>
2023-03-31 15:20:30 +0200

    End Scenes added plus functionality

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,6 +2,7 @@ using Assets.Scripts.AI.State_Machine.Demo_StateMachine;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 
@@ -16,8 +17,14 @@ namespace Assets.Scripts
         public Dictionary<string, int> Scores = new Dictionary<string, int>() { { "Team 1", 0 }, { "Team 2", 0 } };
         public Color TeamOneColor = Color.red;
         public Color TeamTwoColor = Color.blue;
+        
+        //TextMeshProUGUI to show the score on the UI
         public TextMeshProUGUI scoreTeam1;
         public TextMeshProUGUI scoreTeam2;
+        
+        //Integers to hold the values of the scores for each team , will be used to change the scene when either team wins
+        public int scoreTeamRed;
+        public int scoreTeamBlue;
 
         public static GameManager Instance
 
@@ -56,8 +63,25 @@ namespace Assets.Scripts
         // Update is called once per frame
         void Update()
         {
+            //Updates the UI score for each team
             scoreTeam2.text = GameManager.Instance.Scores["Team 1"].ToString();
             scoreTeam1.text = GameManager.Instance.Scores["Team 2"].ToString();
+
+            scoreTeamRed= GameManager.Instance.Scores["Team 1"];
+            scoreTeamBlue = GameManager.Instance.Scores["Team 2"];
+            
+            //If Scores of red team reach 20 or greater, play the end screen for red team
+            if (scoreTeamRed >= 20)
+            {
+                SceneManager.LoadScene("EndScreenRed");
+            }
+            
+            //If Scores of blue team reach 20 or greater, play the end screen for blue team
+            if (scoreTeamBlue >= 20)
+            {
+                SceneManager.LoadScene("EndScreenBlue");
+            }
+            
         }
 
     }
